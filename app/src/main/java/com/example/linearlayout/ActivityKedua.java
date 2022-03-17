f28b707cfecdcc88524203514db1887480f533cb
akbarangrh22 <anugrahakbar522@gmail.com>
2022-03-17 11:59:00 +0700

    menu dan floating button

---
+++ app/src/main/java/com/example/linearlayout/ActivityKedua.java
@@ -0,0 +1,63 @@
+package com.example.linearlayout;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityKedua extends AppCompatActivity {
+    //Mendeklarasikan variabel dengan tipe data TextView
+    TextView txEmail, txPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kedua);
+
+        //Menghubungkan variabel txEmail dengan componen TextView pada layout
+        txEmail = findViewById(R.id.tvEmail);
+
+        //Menghubungkan variabel txEmail dengan componen TextView pada layout
+        txPassword = findViewById(R.id.tvPassword);
+
+        //mendeklarasikan variabel bundle yang akan digunakan untuk mengambil pesan yang akan dikirimkan melalui method intent
+        Bundle bundle = getIntent().getExtras();
+
+        //membuat variabel string yang digunakan untuk menyimpan data yang dikirimkan dari activity sebelumnya dengan kunci "a"
+        String email = bundle.getString("a");
+
+        //membuat variabel string yang digunakan untuk menyimpan data yang dikirimkan dari activity sebelumnya dengan kunci "b"
+        String pass = bundle.getString("b");
+
+        //menampilkan value dari email kedalam txEmail
+        txEmail.setText(email);
+
+        //menampilkan value dari variabel pass kedalam txPassword
+        txPassword.setText(pass);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Method untuk menampilkan menu.
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Membuat kondisi jika yang dipilih adalah id mnDaftar
+        if (item.getItemId() == R.id.mnDaftar) {
+            //Method untuk memanggil activity "Daftar Activity"
+            Intent i = new Intent(getApplicationContext(), DaftarActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
